55ba89b0ad5f5fb2f9e79e4857d7bcb7a405b8d1
Arvind-Kumar-Mahato <93374171+Arvind-Kumar-Mahato@users.noreply.github.com>
2022-07-14 00:58:01 +0530

    search bar added

---
+++ src/Components/UI/SearchBar.jsx
@@ -0,0 +1,98 @@
+import React from 'react'
+import  { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+
+const SearchBar = () => {
+    var apikey =`5039632cb832ec1ad066b935929e983a`;
+    const [currentCity, setCurrentCity] = useState("");
+    const [coordinates, setCoordinates] = useState({});
+
+
+
+    useEffect(()=>{
+        navigator.geolocation.getCurrentPosition(
+          ({ coords: { latitude, longitude } }) => {
+            setCoordinates({ lat: latitude, lng: longitude });
+          }
+        );
+      },[])  
+      useEffect(()=>{
+        if (coordinates.lat !== undefined) {
+  
+            axios
+              .get(
+                `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lng}&appid=${apikey}`
+              )
+              .then(({ data }) => {
+                setCurrentCity(data.name);
+                
+              })
+              .catch((err) => console.log(err));
+          
+          datas(coordinates.lat,coordinates.lng)
+        }
+      },[coordinates])
+  
+  
+  
+    function handleInputChange(event) {
+      setCurrentCity(event.target.value);
+    }
+  
+  
+    function handleButtonClick() {
+      
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${apikey}`)
+      .then(({data})=>{
+         
+      let lat=data.coord.lat;
+      let lon=data.coord.lon;
+          datas(lat,lon)   
+      }) 
+  
+    
+    }
+  
+    function datas(lat, lon){
+      axios.get(`
+      https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${"dfbfa90f298297bd32d78348f443a400"}`)
+  .then(({data})=>
+  {
+    console.log(data)
+    
+    
+  })
+  }
+  
+  
+  
+    function handleKeyPress(e) {
+      if (e.key === "Enter") handleButtonClick();
+    }
+  return (
+    <div className="Search">
+      <div className="location_img">
+        <img src="https://as2.ftcdn.net/v2/jpg/02/72/89/67/1000_F_272896745_tlTivOH81qWIVzz34KqFGm8LO3N9hMYQ.jpg" alt = "" className="location" />
+      </div>
+      <div className="input_box">
+        <input
+          className="input"
+          value={currentCity}
+          onChange={handleInputChange}
+          onKeyPress={handleKeyPress}
+          placeholder = "Search for City"
+        />
+      </div>
+      <div className="search_img">
+        <img
+          className="search_btn"
+          onClick={handleButtonClick}
+          src="https://cdn-icons-png.flaticon.com/512/151/151773.png"  alt =""/> 
+
+      </div>
+    </div>
+  )
+}
+
+export default SearchBar
